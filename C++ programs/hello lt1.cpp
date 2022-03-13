0717ac4a83a5ff2c278b8c352019bb651fe0e69a
arifaggarwal <96463804+arifaggarwal@users.noreply.github.com>
2022-03-13 13:45:58 +0530

    Delete C++ programs directory

--- C++ programs/hello lt1.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>   //header files between <> and "" 
-using namespace std;  //logical region in which we define functions
-int main() 
-{
-    cout<<"hello";  //(printf was a function) in c lang but cout is an object << is insertion operater 
-    return 0;
-}
--- C++ programs/lt 1c.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-//using namespace std;
-int main()
-{
-	int i;
-	std::cin>>i;// cin console input
-	std::cout<<i;// cout console output
-}
